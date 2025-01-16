7cce688dbafeed28e30d93022b687d3de0de66d6
Eduard Badea <eduardbadea@outlook.com>
2025-01-16 18:48:28 +0200

    Update

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 const tenSecondsInterval = 10000;
-let index = 0;
+let index = 1;
 
 function changeLightColour() {
     const colours = ['btn-danger', 'btn-warning', 'btn-success'];
     const button = document.getElementById('button-colour');
     let colourClass= button.getAttribute('class').substring(4);
-    if (index < 2) {
-        button.classList.replace(colourClass, colours[index + 1]);
-        ++index;
-    } else {
-        index = 0;
-        button.classList.replace(colourClass, colours[index]);
-    }
-}
+    console.log(index)
+    button.classList.replace(colourClass, colours[index])
+    index = (index + 1) % 3;
+} 
 
 setInterval(changeLightColour, tenSecondsInterval)
