bd7ae0217e575f2ee6a616c3e130f199bdcdf7c9
Eduard Badea <eduardbadea@outlook.com>
2025-01-16 16:56:50 +0200

    Update

--- script.js
+++ script.js
@@ -4,13 +4,13 @@ let index = 0;
 function changeLightColour() {
     const colours = ['btn-danger', 'btn-warning', 'btn-success'];
     const button = document.getElementById('button-colour');
-    let clasa = button.getAttribute('class').substring(4);
+    let colourClass= button.getAttribute('class').substring(4);
     if (index < 2) {
-        button.classList.replace(clasa, colours[index + 1]);
+        button.classList.replace(colourClass, colours[index + 1]);
         ++index;
     } else {
         index = 0;
-        button.classList.replace(clasa, colours[index]);
+        button.classList.replace(colourClass, colours[index]);
     }
 }
 
