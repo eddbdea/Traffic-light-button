a706d510d753780bf2032cc833355808b0f85c04
Eduard Badea <eduardbadea@outlook.com>
2025-01-17 16:52:03 +0200

    Update delete console.log/script.js

--- script.js
+++ script.js
@@ -5,9 +5,8 @@ function changeLightColour() {
     const colours = ['btn-danger', 'btn-warning', 'btn-success'];
     const button = document.getElementById('button-colour');
     let colourClass= button.getAttribute('class').substring(4);
-    console.log(index)
-    button.classList.replace(colourClass, colours[index])
+    button.classList.replace(colourClass, colours[index]);
     index = (index + 1) % 3;
 } 
 
-setInterval(changeLightColour, tenSecondsInterval)
+setInterval(changeLightColour, tenSecondsInterval);
